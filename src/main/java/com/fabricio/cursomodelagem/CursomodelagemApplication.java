7813f10fc02e514daef63c0033d79b1f6f38fec2
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-05-13 11:05:01 -0300

    Projeto criado

---
+++ src/main/java/com/fabricio/cursomodelagem/CursomodelagemApplication.java
@@ -0,0 +1,13 @@
+package com.fabricio.cursomodelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomodelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomodelagemApplication.class, args);
+	}
+
+}
